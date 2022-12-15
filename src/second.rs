d69bbdc3c7010faa9dc08ec902ebea9da9a79ea7
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-15 15:03:20 +0100

    AoC 2_1

--- src/second.rs
+++ src/second.rs
@@ -1,62 +1,39 @@
 use std::cmp::Ordering;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
 
-
-struct Shape {
-    encrypted: char,
-    value: u32
-}
-
-
-
-impl Eq for Shape {}
-
-impl PartialEq<Self> for Shape {
-    fn eq(&self, other: &Self) -> bool {
-        self.encrypted == other.encrypted
-    }
-}
-
-impl PartialOrd<Self> for Shape {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for Shape {
-    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
-        self.value.cmp(&other.value)
-    }
-}
 
 pub fn solution_a() -> u32 {
-    let scores: HashMap<char, u32> = HashMap::from([
-        ('A', 1),
-        ('B', 2),
-        ('C', 3),
-        ('X', 1),
-        ('Y', 2),
-        ('Z', 3)]);
+    let file = File::open("inputs/2").unwrap();
+    let reader = BufReader::new(file);
+
+
+    let scores: HashMap<String, u32> = HashMap::from([
+        (String::from("A X"), 4),
+        (String::from("A Y"), 8),
+        (String::from("A Z"), 3),
+        (String::from("B X"), 1),
+        (String::from("B Y"), 5),
+        (String::from("B Z"), 9),
+        (String::from("C X"), 7),
+        (String::from("C Y"), 2),
+        (String::from("C Z"), 6)]);
 
     let mut sum:u32 = 0;
 
-    for line in include_str!("../inputs/2_test").lines() {
-        let mut split = line.split_whitespace();
-        let (his, my) = (split.next().unwrap().chars().next().unwrap(), split.next().unwrap().chars().next().unwrap());
-        println!("his: {}, my: {}", his, my);
-        let his = Shape{ encrypted: his, value: *scores.get(&his).unwrap() };
-        let my_value = *scores.get(&my).unwrap();
-        let my = Shape{ encrypted: my, value: my_value };
+    for line in reader.lines() {
 
-        sum += my_value;
+        let line_u = match line {
+            Ok(l) => l,
+            Err(_) => continue
+        };
 
-        match my.cmp(&his) {
-            Ordering::Greater => sum += 6,
-            Ordering::Equal => sum += 3,
-            _ => continue
-        }
+        let total = *scores.get(&line_u).unwrap();
+        sum += total;
 
     }
+
 
     return sum;
 }
