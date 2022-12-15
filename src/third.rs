5974a835d3bd04353cf948041b3c527839adc8a1
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-15 20:52:19 +0100

    AoC 3_1

--- src/third.rs
+++ src/third.rs
@@ -9,29 +9,24 @@ pub fn solution_a() -> i32 {
     let mut sum = 0;
 
     for line in reader.lines() {
-        let mut first_set = HashSet::new();
-        let mut second_set = HashSet::new();
 
         let line = line.unwrap();
         let (first, second) = line.split_at(line.len()/2);
 
-        for i in 0..first.len(){
-            let first_val = first.chars().nth(i).unwrap();
-            let second_val = second.chars().nth(i).unwrap();
-            if second_set.contains(&first_val) {
-                let abs_val = first_val as i32;
+        let first: HashSet<char> = HashSet::from_iter(first.chars());
+        let second: HashSet<char> = HashSet::from_iter(second.chars());
+
+        let common = first
+            .intersection(&second)
+            .next();
+
+        match common {
+            Some(c) => {
+                let abs_val = *c as i32;
                 let val = if abs_val < 97 { abs_val - 38} else { abs_val - 96};
                 sum += val;
-                break;
-            }
-            if first_set.contains(&second_val) {
-                let abs_val = second_val as i32;
-                let val = if abs_val < 97 { abs_val - 38} else { abs_val - 96};
-                sum += val;
-                break;
-            }
-            first_set.insert(first_val);
-            second_set.insert(second_val);
+            },
+            None => continue,
         }
     }
 
