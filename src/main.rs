5a221d08c1577bd0827765d375e5bd9cc583d893
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-15 18:32:47 +0100

    AoC 3_1

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,7 @@ fn main() {
     // println!("Most calories: {}",first::solution_a());
     // println!("Top three elves calories: {}",first::solution_b());
     // println!("Score: {}",second::solution_a());
-    println!("Score: {}",second::solution_b());
+    // println!("Score: {}",second::solution_b());
+    println!("Letter: {}",third::solution_a());
 }
+
