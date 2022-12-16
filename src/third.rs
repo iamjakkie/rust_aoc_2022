5c58ab5fb3821f11cc71486d9b5e9a1d827f50f7
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-16 08:52:55 +0100

    AoC 3_2

--- src/third.rs
+++ src/third.rs
@@ -34,7 +34,7 @@ pub fn solution_a() -> i32 {
 }
 
 pub fn solution_b() -> i32 {
-    let file = File::open("inputs/3_test").unwrap();
+    let file = File::open("inputs/3").unwrap();
     let reader = BufReader::new(file);
 
     let mut cnt = 0;
@@ -44,15 +44,34 @@ pub fn solution_b() -> i32 {
     let mut vec:Vec<String> = Vec::new();
 
     for line in reader.lines() {
-        if cnt % 3 == 0 {
-            // sum += curr_sum;
-            // curr_sum = 0;
-            cnt = 0;
-            println!("{:?}", vec);
-            vec = Vec::new();
-        }
+        cnt += 1;
         vec.push(line.unwrap());
         cnt += 1;
+        if cnt % 3 == 0 {
+            let first:HashSet<char> = HashSet::from_iter(vec[0].clone().chars());
+            let second:HashSet<char> = HashSet::from_iter(vec[1].clone().chars());
+            let third:HashSet<char> = HashSet::from_iter(vec[2].clone().chars());
+
+            let mut common_set = first
+                .intersection(&second)
+                .cloned()
+                .collect::<HashSet<char>>();
+            let common = common_set
+                .intersection(&third)
+                .next();
+
+            match common {
+                Some(c) => {
+                    let abs_val = *c as i32;
+                    let val = if abs_val < 97 { abs_val - 38} else { abs_val - 96};
+                    sum += val;
+                },
+                None => continue,
+            }
+
+            cnt =0;
+            vec = Vec::new();
+        }
     }
 
     return sum;
