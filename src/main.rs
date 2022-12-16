5a46098e9a847fcf346b007ac87756dc4f2f0fed
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-16 08:40:53 +0100

    AoC 3_1

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     // println!("Top three elves calories: {}",first::solution_b());
     // println!("Score: {}",second::solution_a());
     // println!("Score: {}",second::solution_b());
-    println!("Sum of priorities: {}",third::solution_a());
+    // println!("Sum of priorities: {}",third::solution_a());
+    println!("Sum of priorities: {}",third::solution_b());
 }
 
--- src/third.rs
+++ src/third.rs
@@ -32,3 +32,28 @@ pub fn solution_a() -> i32 {
 
     return sum;
 }
+
+pub fn solution_b() -> i32 {
+    let file = File::open("inputs/3_test").unwrap();
+    let reader = BufReader::new(file);
+
+    let mut cnt = 0;
+    let mut sum = 0;
+    let mut curr_sum = 0;
+
+    let mut vec:Vec<String> = Vec::new();
+
+    for line in reader.lines() {
+        if cnt % 3 == 0 {
+            // sum += curr_sum;
+            // curr_sum = 0;
+            cnt = 0;
+            println!("{:?}", vec);
+            vec = Vec::new();
+        }
+        vec.push(line.unwrap());
+        cnt += 1;
+    }
+
+    return sum;
+}
