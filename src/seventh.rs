85a735f512f72a827f9f4ab211ff7492213d46d5
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-21 18:04:46 +0100

    AoC 7_1

--- src/seventh.rs
+++ src/seventh.rs
@@ -1,43 +1,48 @@
-use std::cell::{Ref, RefCell};
+use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-use std::rc::Rc;
-
-struct Node {
-    stats: i32,
-    children: Option<Vec<Node>>,
-    parent: Rc<RefCell<Node>>,
-}
-
-impl Node {
-    fn new() -> Node {
-        Node {
-            stats: 0,
-            children: None,
-            parent: Rc::new(RefCell::new(Node::new())),
-        }
-    }
-
-    fn add_child(&mut self, child: Node) {
-        match self.children {
-            Some(ref mut children) => children.push(child),
-            None => self.children = Some(vec![child]),
-        }
-    }
-
-    fn add_stats(&mut self, stats: i32) {
-        self.stats += stats;
-    }
-
-    fn add_parent(&mut self, parent: Node) {
-        self.parent = Rc::new(RefCell::new(parent));
-    }
-}
+//
+// #[derive(Clone)]
+// struct Node {
+//     stats: i32,
+//     children: Option<RcVec<Node>>,
+//     parent: Option<Box<Node>>
+// }
+//
+// impl Node {
+//     fn new() -> Node {
+//         Node {
+//             stats: 0,
+//             children: None,
+//             parent: None,
+//         }
+//     }
+//
+//     fn add_child(&mut self, child: Node) {
+//         match self.children {
+//             Some(ref mut children) => children.push(child),
+//             None => self.children = Some(vec![child]),
+//         }
+//     }
+//
+//     fn add_stats(&mut self, stats: i32) {
+//         self.stats += stats;
+//     }
+//
+//     fn add_parent(&mut self, parent: Node) {
+//         self.parent = Some(Box::new(parent));
+//     }
+// }
 
 pub fn solution_a() -> i32 {
     let file = File::open("inputs/7_test").unwrap();
     let reader = BufReader::new(file);
+
+    let mut curr_node:char = '\t';
+    let mut visited:Vec<char> = vec!['/'];
+    let mut sizes:HashMap<char, i32> = HashMap::new();
+
 
     for line in reader.lines() {
         println!("line: {:?}", line);
@@ -49,66 +54,58 @@ pub fn solution_a() -> i32 {
 
         let path = split.next();
 
-        let mut curr_node = Node::new();
 
         match path {
             Some(path) => {
-                let curr_node =
                 match path {
+                    "/" => {
+                        curr_node = path.parse().unwrap();
+                    }
                     ".." => {
-                        curr_node = *curr_node.parent.get_mut();
+                        curr_node = visited.pop().unwrap();
                     },
                     _ => {
-                        let mut new_node = Node::new();
-                        new_node.add_parent(curr_node);
-                        curr_node.add_child(new_node);
-                        let curr_node = new_node;
+                        curr_node = path.parse().unwrap();
+                        visited.push(curr_node);
                     }
                 };
-
-            }
-
-            //     match (first, second, path) {
-            //         ("$", "cd", _) => {
-            //
-            //
-            //             match rel_path {
-            //                 ".." => {
-            //                     traverse_path.remove(0);
-            //                 }
-            //                 "." => {}
-            //                 _ => {
-            //                     traverse_path.push(rel_path);
-            //                     path_stats.insert(rel_path.chars().next().unwrap(), 0);
-            //                 }
-            //             }
-            //             println!("cd");
-            //             println!("{:?}", traverse_path);
-            //         },
-            //         ("$", "ls") => {
-            //             println!("ls");
-            //         },
-            //         ("dir", _) => {
-            //             println!("dir");
-            //         },
-            //         (_, _) => {
-            //             println!("unknown");
-            //             let curr_dir = traverse_path.last().unwrap();
-            //             let curr_dir = curr_dir.chars().next().unwrap();
-            //             let size = split.next().unwrap();
-            //             let size = (size).parse::<u32>().unwrap();
-            //             let mut curr_size = path_stats.get_mut(&curr_dir).unwrap();
-            //             *curr_size += size;
-            //         }
-            //     }
-            // }
-            None => (),
+            },
+            None => {
+                let val = first.parse::<i32>();
+                match val {
+                    Ok(val) => {
+                        for node in visited.iter() {
+                            let mut size = sizes.get_mut(node);
+                            match size {
+                                Some(size) => {
+                                    *size += val;
+                                },
+                                None => {
+                                    sizes.insert(*node, val);
+                                }
+                            }
+                        }
+                    },
+                    Err(_) => {
+                        println!("Error parsing val: {:?}", second);
+                    }
+                }
+            },
         };
 
-
-
+        println!("curr_node: {:?}", curr_node);
+        println!("sizes: {:?}", sizes);
     }
-    0
+
+    let mut size = 0;
+
+    for i in sizes.values() {
+        if i < &100000 {
+            size += i;
+        }
+    }
+
+    size
 }
 
 
