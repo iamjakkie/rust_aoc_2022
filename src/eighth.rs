e1250255fbe6e708a5d232706638a3e1691fd398
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-21 20:35:23 +0100

    AoC 8_1

---
+++ src/eighth.rs
@@ -0,0 +1,11 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+pub fn solution_a() -> i32 {
+    let file = File::open("inputs/8_test").unwrap();
+    let reader = BufReader::new(file);
+
+    for line in reader.lines():
+
+    0
+}
