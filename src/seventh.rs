53e0e5586ac300258919be32671086721839a0e5
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-23 21:12:37 +0100

    AoC 7_1

--- src/seventh.rs
+++ src/seventh.rs
@@ -1,111 +1,69 @@
-use std::borrow::BorrowMut;
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
-//
-// #[derive(Clone)]
-// struct Node {
-//     stats: i32,
-//     children: Option<RcVec<Node>>,
-//     parent: Option<Box<Node>>
-// }
-//
-// impl Node {
-//     fn new() -> Node {
-//         Node {
-//             stats: 0,
-//             children: None,
-//             parent: None,
-//         }
-//     }
-//
-//     fn add_child(&mut self, child: Node) {
-//         match self.children {
-//             Some(ref mut children) => children.push(child),
-//             None => self.children = Some(vec![child]),
-//         }
-//     }
-//
-//     fn add_stats(&mut self, stats: i32) {
-//         self.stats += stats;
-//     }
-//
-//     fn add_parent(&mut self, parent: Node) {
-//         self.parent = Some(Box::new(parent));
-//     }
-// }
+use regex::Regex;
+
+
+#[derive(Debug)]
+struct Dir{
+    path: String,
+    size: usize,
+    parent: usize
+}
 
 pub fn solution_a() -> i32 {
     let file = File::open("inputs/7").unwrap();
     let reader = BufReader::new(file);
 
-    let mut curr_node:String = String::new();
-    let mut visited:Vec<String> = Vec::new();
-    let mut sizes:HashMap<String, i32> = HashMap::new();
+    let mut dirs:Vec<Dir> = vec![Dir {
+        path: "/".to_string(),
+        size: 0,
+        parent: 0
+    }];
+    let root = 0;
+
+    let mut curr = root;
+
+    let directory_regex = Regex::new(r"\s*\$ cd (P<target>.*)").unwrap();
+    let file_regex = Regex::new(r"\s*(?P<size>\d+)\s+(?P<file>.+)").unwrap();
 
 
     for line in reader.lines() {
-        let mut line = line.unwrap();
-        let mut split = line.split(" ");
-
-
-        let (first, second) = (split.next().unwrap(), split.next().unwrap());
-
-        let path = split.next();
-
-
-        match path {
-            Some(path) => {
-                let path = String::from(path);
-                match path.as_str() {
-                    ".." => {
-                        curr_node = visited.pop().unwrap();
-                        // curr_node = visited.last_mut().unwrap().parse().unwrap();
-                    },
-                    _ => {
-                        curr_node = path;
-                        visited.push(curr_node);
-                    }
-                };
-            },
-            None => {
-                let val = first.parse::<i32>();
-                match val {
-                    Ok(val) => {
-                        for i in 0..visited.len() {
-                            let k = visited[i].clone();
-                            let mut size = sizes.get_mut(&k);
-                            match size {
-                                Some(size) => {
-                                    *size += val;
-                                },
-                                None => {
-                                    let k = visited[i].clone();
-                                    sizes.insert(k, val);
-                                }
-                            }
-                        }
-                    },
-                    Err(_) => {
-                        continue;
-                        // println!("Error parsing val: {:?}", second);
+        match directory_regex.captures(line.unwrap().as_str()) {
+            Some(cap) => {
+                match &cap["target"] {
+                    "/" => curr = root,
+                    ".." => curr = dirs[curr].parent,
+                    name => {
+                        dirs.push(Dir {
+                            path: name.to_string(),
+                            size: 0,
+                            parent: curr
+                        });
+                        curr = dirs.len() - 1;
                     }
                 }
             },
-        };
-    println!("sizes: {:?}", sizes);
-
-    }
-
-    let mut size = 0;
-
-    for i in sizes.values() {
-        if *i <= 100000 {
-            size += i;
+            None => {
+                match file_regex.captures(line.unwrap().as_str()) {
+                    Some(cap) => {
+                        let size = &cap["size"].parse().unwrap();
+                        let mut p = curr;
+                        loop {
+                            dirs[p].size += size;
+                            if dirs[p].parent == p {
+                                break;
+                            }
+                            p = dirs[p].parent;
+                        }
+                    },
+                    None => {}
+                }
+            }
         }
     }
 
-    size
+    0
 }
 
 
