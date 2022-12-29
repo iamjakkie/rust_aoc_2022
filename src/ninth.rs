baec5812c79cb0e73bd93ba195bc2e34ce5f4e94
jakkie <jakubkielbasiewicz@gmail.com>
2022-12-29 16:00:10 +0100

    AoC 9_1

--- src/ninth.rs
+++ src/ninth.rs
@@ -2,6 +2,19 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 use std::collections::HashSet;
 
+/*
+
+R->U -> skip last R
+R->D -> skip last R
+L->U -> skip last L
+L->D -> skip last L
+U->R -> skip last U
+U->L -> skip last U
+D->R -> skip last D
+D->L -> skip last D
+
+ */
+
 pub fn solution_a() -> i32 {
     let file = File::open("inputs/1").unwrap();
     let reader = BufReader::new(file);
@@ -15,6 +28,8 @@ pub fn solution_a() -> i32 {
         let line = line.unwrap();
         let mut split = line.split(" ");
 
+
+
         visited.insert(pos);
 
         let (dir, num) = (split.next().unwrap(), split.next().unwrap());
@@ -25,6 +40,7 @@ pub fn solution_a() -> i32 {
             ("R", n) => {
                 for i in [pos[0]..n] {
                     pos[0] += 1;
+                    visited.insert(pos);
                 }
             },
             ("L", n) => {
