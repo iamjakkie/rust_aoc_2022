9332029013b8c7b253338a4fafa365fe0b8a7b86
jakkie <jakubkielbasiewicz@gmail.com>
2023-01-01 22:01:59 +0100

    AoC 9_2

--- src/ninth.rs
+++ src/ninth.rs
@@ -6,57 +6,42 @@ fn get_head_path() -> Vec<[i32;2]> {
     let file = File::open("inputs/9_test").unwrap();
     let reader = BufReader::new(file);
 
-    let mut head_visited: Vec<[i32; 2]> = Vec::new();
-
-    let mut head = [0, 4];
-
-    head_visited.push(head);
+    let mut moves: Vec<[i32; 2]> = Vec::new();
 
     for line in reader.lines() {
         let line = line.unwrap();
-        let mut split = line.split(" ");
 
-        let (dir, num) = (split.next().unwrap(), split.next().unwrap());
-
-        let (vertical, opposite, slice) = match (dir, num.parse::<i32>().unwrap()) {
-            ("R", n) => (false, false, (0..n).collect::<Vec<i32>>()),
-            ("L", n) => (false, true, (0..n).collect::<Vec<i32>>()),
-            ("U", n) => (true, true, (0..n).collect::<Vec<i32>>()),
-            ("D", n) => (true, false, (0..n).collect::<Vec<i32>>()),
-            _ => {
-                continue;
-            }
-        };
-
-        match (vertical, opposite) {
-            (true, true) => {
-                for _ in slice {
-                    head[1] -= 1;
-                    head_visited.push(head);
-                }
-            }
-            (true, false) => {
-                for _ in slice {
-                    head[1] += 1;
-                    head_visited.push(head);
-                }
-            }
-            (false, true) => {
-                for _ in slice {
-                    head[0] -= 1;
-                    head_visited.push(head);
-                }
-            }
-            (false, false) => {
-                for _ in slice {
-                    head[0] += 1;
-                    head_visited.push(head);
+        for (dir, count) in line.trim().split_once(" "){
+            for _ in 0..count.parse().unwrap() {
+                match dir {
+                    "R" => {moves.push([1,0])},
+                    "L" => {moves.push([-1,0])},
+                    "U" => {moves.push([0,1])},
+                    "D" => {moves.push([0,-1])},
+                    _ => {continue;}
                 }
             }
         }
     }
 
-    head_visited
+    moves
+}
+
+fn has_to_move(curr:&[i32;2], neighbour:&[i32;2]) -> bool {
+    let x_diff = curr[0].abs_diff(neighbour[0]);
+    let y_diff = curr[1].abs_diff(neighbour[1]);
+
+    if (x_diff < 2 && y_diff < 2) {
+        false;
+    }
+    true
+}
+
+fn make_moves(moves: Vec<[i32;2]>, len: usize) {
+    let mut snake:Vec<[i32;2]> = vec![[0,0];len];
+
+    let mut visited:Vec<[i32;2]> = Vec::from([[0,0]]);
+
 }
 
 pub fn solution_a() -> i32 {
@@ -72,9 +57,9 @@ pub fn solution_a() -> i32 {
             break;
         }
         let cell = head_visited[ind];
-        let x_diff = cell[0].abs_diff(tail[0]);
-        let y_diff = cell[1].abs_diff(tail[1]);
-        if (x_diff < 2 && y_diff < 2) {
+        let to_move = has_to_move(&tail, &cell);
+
+        if to_move {
             last = cell;
             ind += 1;
             continue;
@@ -105,17 +90,19 @@ pub fn solution_b() -> i32 {
 
     for cell in head_visited {
         last_snake = snake[snake.len() - 1];
-        let x_diff = cell[0].abs_diff(last_snake[0]);
-        let y_diff = cell[1].abs_diff(last_snake[1]);
+        let to_move = has_to_move(&cell, &last_snake);
         // println!("Last: {:?}, curr: {:?}, to_insert: {:?}", last_snake, cell, last);
 
-        if (x_diff < 2 && y_diff < 2) {
+        if to_move {
             last = cell;
             continue;
         } else {
             visited.push(snake[0]);
+            let mut prev;
             for val in snake.iter().rev() {
+                if prev {
 
+                }
             }
             let mut snake_temp:[[i32;2];9] = [[0;2];9];
             snake_temp[0..8].copy_from_slice(&snake[1..]);
